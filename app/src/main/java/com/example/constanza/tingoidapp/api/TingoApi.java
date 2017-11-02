7df56b44cc46bc39e253041d8c5ba79315d83c3e
Unknown <cisc.amaro@gmail.com>
2017-11-02 13:34:39 -0300

    Auto stash before merge of "master" and "origin/master"
    Todo

--- app/src/main/java/com/example/constanza/tingoidapp/api/TingoApi.java
+++ app/src/main/java/com/example/constanza/tingoidapp/api/TingoApi.java
@@ -28,7 +28,7 @@ public interface TingoApi {
     // TODO: Cambiar host por "10.0.3.2" para Genymotion.
     // TODO: Cambiar host por "10.0.2.2" para AVD.
     // TODO: Cambiar host por IP de tu PC para dispositivo real.
-    public static final String BASE_URL = "http://10.0.2.2:8000/tingo/";
+    public static final String BASE_URL = "http://10.6.43.212:8000/tingo/";
 
     @GET ("handshaking/")
     Call <ResponseBody> handshaking();
